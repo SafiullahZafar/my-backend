c271c7e1d3b5ea0356f175f85bc0696ec1f31177
Safiullah Zafer <safullahzafar@gmail.com>
2026-02-24 15:33:12 +0500

    Update server.jsmake it to run for versal ok

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 // Socket.io setup
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // Vite default
+    origin: 'protasksfrontend-updates.vercel.app', // Vite default
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 });
